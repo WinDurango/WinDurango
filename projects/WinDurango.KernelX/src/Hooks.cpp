a931da33027b63b9ccdf88fbdf7ce9b87680ce00
CT5 <124994670+Cherrytree56567@users.noreply.github.com>
2026-06-07 15:13:28 +1000

    fix: fixed Minecraft 1.2.0.0

--- projects/WinDurango.KernelX/src/Hooks.cpp
+++ projects/WinDurango.KernelX/src/Hooks.cpp
@@ -330,15 +330,17 @@ HRESULT WINAPI WdRoGetActivationFactoryCore(
     {
         HRESULT hr = g_RoGetActivationFactory(activatableClassId, iid, factory);
 
-		if (FAILED(hr))
-			return hr;
+        if (FAILED(hr))
+            return hr;
 
-		TrueActivateInstance = GetVTableMethod<decltype(TrueActivateInstance)>(*factory, 6);
+        if (!TrueActivateInstance) {
+            TrueActivateInstance = GetVTableMethod<decltype(TrueActivateInstance)>(*factory, 6);
 
-		DetourTransactionBegin();
-		DetourUpdateThread(GetCurrentThread());
-        DetourAttach(reinterpret_cast<PVOID*>(&TrueActivateInstance), EraAppActivateInstance);
-		DetourTransactionCommit();
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            DetourAttach(reinterpret_cast<PVOID*>(&TrueActivateInstance), EraAppActivateInstance);
+            DetourTransactionCommit();
+        }
     }
 
     for (auto pfn : g_RoEntryPoints)
