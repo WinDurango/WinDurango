394444d100e7f71dd3318bb7001e15866a096d7a
RodX2010 <90097545+RodX2010@users.noreply.github.com>
2026-06-12 17:02:09 -0300

    Storage fix for FM5

--- projects/WinDurango.WinRT/src/Windows/Xbox/Storage/Windows.Xbox.Storage.ConnectedStorage.cpp
+++ projects/WinDurango.WinRT/src/Windows/Xbox/Storage/Windows.Xbox.Storage.ConnectedStorage.cpp
@@ -55,6 +55,11 @@ winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Foundation::Collecti
             data.Insert(blobs, fileBuffer);
         } catch (...) {
             p_wd->log.Warn("WinDurango::WinRT::Windows::Xbox::ConnectedStorage", "File doesnt exist: {}", winrt::to_string(blobs));
+
+            //Forza hangs if this isn't done
+            winrt::Windows::Storage::IStorageFile file = co_await folder.GetFileAsync(blobs);
+            auto fileBuffer = co_await winrt::Windows::Storage::FileIO::ReadBufferAsync(file);
+            data.Insert(blobs, fileBuffer);
         }
     }
     co_await Read(containerName, data.GetView());
